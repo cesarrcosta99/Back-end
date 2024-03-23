7e7f5cfa13c0e693297a4d6c95a2ca12794bfe36
cesarrcosta99 <cesarrcosta99@gmail.com>
2024-03-22 21:55:37 -0300

    testar...

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,12 +1,22 @@
 import multer from 'multer'
 import { v4 } from 'uuid'
-import { extname, resolve } from 'path'
+import { extname, resolve, dirname } from 'path' // Importe dirname junto com as outras funções do módulo path
+import { fileURLToPath } from 'url'
+
+// Obtendo __filename e __dirname equivalentes em módulo ES
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
   storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', 'uploads'),
-    filename: (request, file, callback) => {
-      return callback(null, v4() + extname(file.originalname))
+    destination: (req, file, cb) => {
+      // O uso de __dirname aqui está correto graças ao código acima que define __dirname
+      const uploadFolder = resolve(__dirname, '..', '..', 'uploads')
+      cb(null, uploadFolder)
+    },
+    filename: (req, file, cb) => {
+      // Garantindo que o arquivo terá um nome único
+      cb(null, v4() + extname(file.originalname))
     },
   }),
 }
